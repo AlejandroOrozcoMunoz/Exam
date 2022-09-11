1f76f3255fd9ec72920235da9679b51fb9d8dd8b
Alejandro Orozco Munoz <104179006+AlejandroOrozcoMunoz@users.noreply.github.com>
2022-09-11 17:42:47 -0500

    Add files via upload

---
+++ Belt Exam/js/script.js
@@ -0,0 +1,12 @@
+function eliminaCookie() {
+    var elementoCookie = document.querySelector('.cookie');
+    elementoCookie.remove();
+}
+
+function cart() {
+    var cartoon = cart.innerText
+
+    alert("Your Car is empty!")
+}
+
+
